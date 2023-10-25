aaab210a5032ce3de0e26e205fe9ea0d428d7a18
Nishchala Namburi <142998991+Nishchala03@users.noreply.github.com>
2023-10-25 15:14:27 -0500

    modified users

---
+++ routes/computation.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+router.get('/', function(req, res, next) {
+     var x = Math.round(Math.random()*10);
+    var z = Math.round(Math.random()*20);
+    var result = Math.sin(x,z); res.setHeader('Content-Type', 'text/html');
+    res.send( `f(sin(${x},${z})) is ${result}`);
+});
+    module.exports = router;
